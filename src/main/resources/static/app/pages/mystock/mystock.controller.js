bf96f0f76bd5f0406e950d87e6512bf31c74d796
FSOFT.FPT.VN\DuyDT2 <DuyDT2@fsoft.com.vn>
2018-05-30 16:26:57 +0700

    Function search in FE

--- src/main/resources/static/app/pages/mystock/mystock.controller.js
+++ src/main/resources/static/app/pages/mystock/mystock.controller.js
@@ -9,4 +9,16 @@ appPages.controller('mystockController', function($scope, $http) {
             alert("Cannot get all goods......");
             console.log("Unable to perform get request export");
         });
+    
+    
+    $scope.searchData   = '';     // set the default search/filter term
+    
+    // create the list of sushi rolls 
+//    $scope.sushi = [
+//      { name: 'Cali Roll', fish: 'Crab', tastiness: 2 },
+//      { name: 'Philly', fish: 'Tuna', tastiness: 4 },
+//      { name: 'Tiger', fish: 'Eel', tastiness: 7 },
+//      { name: 'Rainbow', fish: 'Variety', tastiness: 6 }
+//    ];
+    
 });
